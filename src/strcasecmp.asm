1be090e18e631c5e58f4552b9208a71687fac212
alacoste <alexandre.lacoste@epitech.eu>
2021-03-01 18:51:31 +0100

    [mod] reg

--- src/strcasecmp.asm
+++ src/strcasecmp.asm
@@ -8,36 +8,36 @@ strcasecmp:
 _incr:
     inc rcx ; increment counter and go in loop
 _loop:
-    mov r14b, BYTE [rdi + rcx]    ; copy in a tmp r14b, byte at pointer rdi + rcx
-    mov r15b, BYTE [rsi + rcx]    ; copy in a tmp r15b, byte at pointer rsi + rcx
+    mov r9b, BYTE [rdi + rcx]    ; copy in a tmp r9b, byte at pointer rdi + rcx
+    mov r10b, BYTE [rsi + rcx]    ; copy in a tmp r10b, byte at pointer rsi + rcx
 
 _check_first:
-    cmp r14b, 0  ; check if r14b == 0
+    cmp r9b, 0  ; check if r9b == 0
     je _check_second  ; if equal -> _return
-    mov bl, r14B    ; bl is a tmp var
+    mov bl, r9B    ; bl is a tmp var
     sub bl, 65    ; sub 65 to bl correspond 'A'
     cmp bl, 25 ; compare if bl equal to 'Z' as with sub 65 (90 - 65)
     ja _check_second   ; jump if above unsigned
-    add r14b, 32 ; set good value in r14b uppercase to lowercase
+    add r9b, 32 ; set good value in r9b uppercase to lowercase
 
 _check_second:
-    cmp r15b, 0  ; check if r15b == 0
+    cmp r10b, 0  ; check if r10b == 0
     je _compare_both  ; if equal -> _return
-    mov bl, r15B    ; bl is a tmp var
+    mov bl, r10B    ; bl is a tmp var
     sub bl, 65    ; sub 65 to bl correspond 'A'
     cmp bl, 25 ; compare if bl equal to 'Z' as with sub 65 (90 - 65)
     ja _compare_both   ; jump if above unsigned
-    add r15b, 32 ; set good value in r14b uppercase to lowercase
+    add r10b, 32 ; set good value in r9b uppercase to lowercase
 
 _compare_both:
-    cmp r14b, 0  ; check if r14b == 0
+    cmp r9b, 0  ; check if r9b == 0
     je _return  ; if equal -> _return
-    cmp r15b, 0  ; check if r15b == 0
+    cmp r10b, 0  ; check if r10b == 0
     je _return  ; if equal -> _return
-    cmp r14b, r15b  ;   compare
+    cmp r9b, r10b  ;   compare
     jne _return ; if not equal -> _return
     jmp _incr
 _return:
-	sub r14d, r15d	; r14d = r14d - r15d, rd14 is 32 bits version of r14b
-    mov eax, r14d   ; copy substract eax (eax because -> 32 bites size of int)
+	sub r9d, r10d	; r9d = r9d - r10d, rd14 is 32 bits version of r9b
+    mov eax, r9d   ; copy substract eax (eax because -> 32 bites size of int)
     ret
--- src/strcmp.asm
+++ src/strcmp.asm
@@ -8,17 +8,17 @@ strcmp:
 _incr:
     inc rcx ; increment counter and go in loop
 _loop:
-    mov r14b, BYTE [rdi + rcx]    ; copy in a tmp r14b, byte at pointer rdi + rcx
-    mov r15b, BYTE [rsi + rcx]    ; copy in a tmp r15b, byte at pointer rsi + rcx
-    cmp r14b, r15b  ;   compare
+    mov r9b, BYTE [rdi + rcx]    ; copy in a tmp r9b, byte at pointer rdi + rcx
+    mov r10b, BYTE [rsi + rcx]    ; copy in a tmp r10b, byte at pointer rsi + rcx
+    cmp r9b, r10b  ;   compare
     jne _return ; if not equal -> _return
-    cmp r14b, 0  ; check if r14b == 0
+    cmp r9b, 0  ; check if r9b == 0
     je _return  ; if equal -> _return
-    cmp r15b, 0  ; check if r15b == 0
+    cmp r10b, 0  ; check if r10b == 0
     je _return  ; if equal -> _return
     jmp _incr
 
 _return:
-	sub r14d, r15d	; r14d = r14d - r15d, rd14 is 32 bits version of r14b
-    mov eax, r14d   ; copy substract eax (eax because -> 32 bites size of int)
+	sub r9d, r10d	; r9d = r9d - r10d, rd14 is 32 bits version of r9b
+    mov eax, r9d   ; copy substract eax (eax because -> 32 bites size of int)
     ret
